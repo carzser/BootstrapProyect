6828872d344ce28363c0ddad734d5df5a228203a
Carlos Serrano <aserranoc@uninorte.edu.co>
2019-08-18 22:28:43 -0500

    Intentando correr localhost con bootstrap. Todavía no funciona

---
+++ Proyecto/app.js
@@ -0,0 +1,41 @@
+/*var http = require('http');
+var fs = require('fs');
+var express = require('express');
+var app = express();
+var router = express.router();
+
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
+function onRequest(request, response){
+    console.log("A user made a request" + request.url);
+    response.writeHead(200, {"Content-Type": "text/html"});
+    fs.readFile('./index.html', null, function(error, data){
+        if(error){
+            response.writeHead(404);
+            response.write("File not found.");
+        }else{
+            response.write(data);
+        }
+        response.end();
+    });
+}
+
+http.createServer(onRequest).listen(3000);
+console.log("Server is now running...");*/
+
+var express = require("express");
+var app = express();
+var router = express.Router();
+var path = __dirname ; // this folder should contain your html files.
+
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
+router.get("/",function(req,res){
+  res.sendFile(path + "/index.html");
+});
+
+app.use("/",router);
+
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
+});
