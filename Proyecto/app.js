0a8f6a81bf9de2b53438656af7bbd13435e02ffe
Juan Solano <juansolanosilva@gmail.com>
2019-08-22 09:33:27 -0500

    Todas las rutas corregidas y bootstrap completamente funcional

--- Proyecto/app.js
+++ Proyecto/app.js
@@ -26,16 +26,18 @@ console.log("Server is now running...");*/
 var express = require("express");
 var app = express();
 var router = express.Router();
-var path = __dirname ; // this folder should contain your html files.
+var path = __dirname; // this folder should contain your html files.
 
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/data_lib', express.static(__dirname + '/data_lib'));
+app.use('/img', express.static(__dirname + '/img'));
 
-router.get("/",function(req,res){
+
+router.get("/", function (req, res) {
   res.sendFile(path + "/index.html");
 });
 
-app.use("/",router);
+app.use("/", router);
 
-app.listen(3000,function(){
+app.listen(3000, function () {
   console.log("Live at Port 3000");
 });
